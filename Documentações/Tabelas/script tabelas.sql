5dc85b35c857e62617673ae62c2c9ba60b634eff
DESKTOP-0IPS8A4\Hebert Talles <talles_be@hotmail.com>
2024-05-16 19:42:34 -0300

    Ajusta informacoes

--- Documentações/Tabelas/script tabelas.sql
+++ Documentações/Tabelas/script tabelas.sql
@@ -14,47 +14,37 @@ GO
 BEGIN TRANSACTION;
 GO
 
-CREATE TABLE [Comandos] (
+CREATE TABLE [Criacao] (
     [Id] uniqueidentifier NOT NULL,
+    [Descricao] VARCHAR(256) NULL,
     [UrlAudio] VARCHAR(MAX) NULL,
     [Transcricao] VARCHAR(MAX) NULL,
     [UrlImagem] VARCHAR(MAX) NULL,
     [InstanteCriacao] DATETIME2 NOT NULL,
     [InstanteAtualizacao] DATETIME2 NOT NULL,
-    CONSTRAINT [PK_Comandos] PRIMARY KEY ([Id])
+    CONSTRAINT [PK_Criacao] PRIMARY KEY ([Id])
 );
 GO
 
-CREATE TABLE [ProcessamentoComandos] (
+CREATE TABLE [ProcessamentosCriacao] (
     [Id] uniqueidentifier NOT NULL,
     [Estado] VARCHAR(20) NOT NULL,
     [InstanteCriacao] DATETIME2 NOT NULL,
-    [MensagemErro] VARCHAR(256) NULL,
-    [ComandoId] uniqueidentifier NOT NULL,
-    CONSTRAINT [PK_ProcessamentoComandos] PRIMARY KEY ([Id]),
-    CONSTRAINT [FK_ProcessamentoComandos_Comandos_ComandoId] FOREIGN KEY ([ComandoId]) REFERENCES [Comandos] ([Id]) ON DELETE CASCADE
+    [MensagemErro] VARCHAR(MAX) NULL,
+    [CriacaoId] uniqueidentifier NOT NULL,
+    CONSTRAINT [PK_ProcessamentosCriacao] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_ProcessamentosCriacao_Criacao_CriacaoId] FOREIGN KEY ([CriacaoId]) REFERENCES [Criacao] ([Id]) ON DELETE CASCADE
 );
 GO
 
-CREATE INDEX [IX_ProcessamentoComandos_ComandoId] ON [ProcessamentoComandos] ([ComandoId]);
+CREATE INDEX [IX_ProcessamentosCriacao_CriacaoId] ON [ProcessamentosCriacao] ([CriacaoId]);
 GO
 
 INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20240315113826_Inicial', N'7.0.16');
+VALUES (N'20240510215734_Inicial', N'7.0.16');
 GO
 
 COMMIT;
 GO
 
-BEGIN TRANSACTION;
-GO
 
-ALTER TABLE [Comandos] ADD [Descricao] VARCHAR(256) NULL;
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20240315151800_Adiciona descricao', N'7.0.16');
-GO
-
-COMMIT;
-GO
